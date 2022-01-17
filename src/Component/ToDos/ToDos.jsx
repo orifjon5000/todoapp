42aa921347a408e53a1ea09b7a0f1bed3630106f
orifjon <orifjon.5000@gmail.com>
2022-01-17 23:38:06 +0900

    test

--- src/Component/ToDos/ToDos.jsx
+++ src/Component/ToDos/ToDos.jsx
@@ -4,11 +4,22 @@ import { useLocation, useParams } from "react-router-dom";
 import AddTodo from "../AddToDo/AddToDo";
 import Mockdata from '../../Mocks/todos.js'
 import ToDoItems from "./ToDoItems";
-
+import { findRenderedComponentWithType } from "react-dom/cjs/react-dom-test-utils.production.min";
+impor axios from '../../utils/axios.js'
 
 export default function ToDos() {
   const location = useLocation();
   const params = useParams();
+
+  const fetchTodos=async()=>{
+    try{
+const data=axios
+    }
+    catch(error){
+
+    }
+  }
+
   return (
     <StyledTodos>
       <h2>Too name</h2>
